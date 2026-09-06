eec6b5cb1d2ded68220ff101a223b908a00647e1
tokindanganael7-hash <tokindanganael7@gmail.com>
2026-09-06 14:49:02 +0100

    Use environment variable for PORT and serve static files
    Updated server to use environment variable for PORT and serve static files.

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Bonjour ! Mon serveur fonctionne 🎉</h1>');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur http://localhost:${PORT}`);
